dbdff6e1cc9a4642f04a0d7550239dde39877d6f
Dani Bilkis <dani.bilkis@gmail.com>
2018-05-30 22:59:30 -0500

    Merge branch 'April_24_2018' of https://github.com/DaniBilkis/Connectica into April_24_2018
    # Conflicts:
    #	src/app/app-routing/routes.ts

--- src/app/transactions/transactions.component.ts
+++ src/app/transactions/transactions.component.ts
@@ -2,6 +2,103 @@ import { Component, OnInit } from '@angular/core';
 import { TransactionDetails } from '../_shared/transaction-details';
 import {TransactionDetailsNotes} from '../_shared/transaction-details-notes';
 
+export const TRANSACTIONS: TransactionDetails[] = [
+  {
+    typeOfOrder: 'Purchase Order',
+    orderNumber: '00776167',
+    documentNumber: '123DXS',
+    documentSender: 'BAHAG Webedi',
+    date: '2017-10-16T17:57:28.556094Z',
+    deliveryStatus: 'Confirmed',
+    supplier: {
+      supplierId: "111776",
+      supplierName: "Wolff Winhouse",
+      supplierAddress: "Guetenbergstrasse 21",
+      supplierSomeNumber: "129992123456"
+    },
+    buyer: {
+      buyerId: "123456ZP",
+      buyerName: 'BAHAG AG',
+      buyerAddress: 'Guetenbergstrasse 21',
+      buyerVAT: 'DE123456778890',
+      buyerSomeNumber: '402456345345345'
+    },
+    shipTo: {
+      name: '',
+      street: '3990 Vitruvian way',
+      city: 'Addison',
+      zip: '75001',
+      country: 'USA',
+      phone: '2145782643',
+      email: 'dani.bilkis@gmail.com'
+    },
+    ultimateCustomer: {
+      name: 'Some Name',
+      street: '3990 Vitruvian way',
+      city: 'Addison',
+      zip: '75001',
+      country: 'USA',
+      phone: '2145782643',
+      email: 'dani.bilkis@gmail.com'
+    },
+    deliveryInfo: {
+      earliestDate: '2018-05-31T17:57:28.556094Z',
+      latestDate: '2018-06-29T17:57:28.556094Z',
+      requestedDate: '2018-06-15T17:57:28.556094Z'
+    },
+    paymentConditions: {
+      paymentDueDate: '2018-06-15T17:57:28.556094Z'
+    }
+  },
+  {
+    typeOfOrder: 'Consigment Order',
+    orderNumber: '00776167',
+    documentNumber: '123DXS',
+    documentSender: 'BAHAG Webedi',
+    date: '2017-10-16T17:57:28.556094Z',
+    deliveryStatus: 'Confirmed',
+    supplier: {
+      supplierId: '111776',
+      supplierName: 'Wolff Winhouse',
+      supplierAddress: 'Guetenbergstrasse 21',
+      supplierSomeNumber: '129992123456'
+    },
+    buyer: {
+      buyerId: '123456ZP',
+      buyerName: 'BAHAG AG',
+      buyerAddress: 'Guetenbergstrasse 21',
+      buyerVAT: 'DE123456778890',
+      buyerSomeNumber: '402456345345345'
+    },
+    shipTo: {
+      name: 'Vasily',
+      street: '3990 Vitruvian way',
+      city: 'Addison',
+      zip: '75001',
+      country: 'USA',
+      phone: '2145782643',
+      email: 'dani.bilkis@gmail.com'
+    },
+    ultimateCustomer: {
+      name: 'Some Name',
+      street: '3990 Vitruvian way',
+      city: 'Addison',
+      zip: '75001',
+      country: 'USA',
+      phone: '2145782643',
+      email: 'dani.bilkis@gmail.com'
+    },
+    deliveryInfo: {
+      earliestDate: '2018-05-31T17:57:28.556094Z',
+      latestDate: '2018-06-29T17:57:28.556094Z',
+      requestedDate: '2018-06-15T17:57:28.556094Z'
+    },
+    paymentConditions: {
+      paymentDueDate: '2018-06-15T17:57:28.556094Z'
+    }
+  }
+];
+
 @Component({
   selector: 'app-transactions',
   templateUrl: './transactions.component.html',
@@ -16,113 +113,22 @@ export class TransactionsComponent implements OnInit {
 
   selectedItem: TransactionDetails;
 
-  transactions: TransactionDetails = [
-    {
-      typeOfOrder: 'Purchase Order',
-      orderNumber: '00776167',
-      documentNumber: '123DXS',
-      documentSender: 'BAHAG Webedi',
-      date: '2017-10-16T17:57:28.556094Z',
-      deliveryStatus: 'Confirmed',
-      supplier: {
-        supplierId: '111776',
-        supplierName: 'Wolff Winhouse',
-        supplierAddress: 'Guetenbergstrasse 21',
-        supplierSomeNumber: '129992123456'
-      },
-      buyer: {
-        buyerId: '123456ZP',
-        buyerName: 'BAHAG AG',
-        buyerAddress: 'Guetenbergstrasse 21',
-        buyerVAT: 'DE123456778890',
-        buyerSomeNumber: '402456345345345'
-      },
-      shipTo: {
-        name: '',
-        street: '3990 Vitruvian way',
-        city: 'Addison',
-        zip: '75001',
-        country: 'USA',
-        phone: '2145782643',
-        email: 'dani.bilkis@gmail.com'
-      },
-      ultimateCustomer: {
-        name: 'Some Name',
-        street: '3990 Vitruvian way',
-        city: 'Addison',
-        zip: '75001',
-        country: 'USA',
-        phone: '2145782643',
-        email: 'dani.bilkis@gmail.com'
-      },
-      deliveryInfo: {
-        earliestDate: '2018-05-31T17:57:28.556094Z',
-        latestDate: '2018-06-29T17:57:28.556094Z',
-        requestedDate: '2018-06-15T17:57:28.556094Z'
-      },
-      paymentConditions: {
-        paymentDueDate: '2018-06-15T17:57:28.556094Z'
-      }
-    },
-    {
-      typeOfOrder: 'Consigment Order',
-      orderNumber: '00776167',
-      documentNumber: '123DXS',
-      documentSender: 'BAHAG Webedi',
-      date: '2017-10-16T17:57:28.556094Z',
-      deliveryStatus: 'Confirmed',
-      supplier: {
-        supplierId: '111776',
-        supplierName: 'Wolff Winhouse',
-        supplierAddress: 'Guetenbergstrasse 21',
-        supplierSomeNumber: '129992123456'
-      },
-      buyer: {
-        buyerId: '123456ZP',
-        buyerName: 'BAHAG AG',
-        buyerAddress: 'Guetenbergstrasse 21',
-        buyerVAT: 'DE123456778890',
-        buyerSomeNumber: '402456345345345'
-      },
-      shipTo: {
-        name: 'Vasily',
-        street: '3990 Vitruvian way',
-        city: 'Addison',
-        zip: '75001',
-        country: 'USA',
-        phone: '2145782643',
-        email: 'dani.bilkis@gmail.com'
-      },
-      ultimateCustomer: {
-        name: 'Some Name',
-        street: '3990 Vitruvian way',
-        city: 'Addison',
-        zip: '75001',
-        country: 'USA',
-        phone: '2145782643',
-        email: 'dani.bilkis@gmail.com'
-      },
-      deliveryInfo: {
-        earliestDate: '2018-05-31T17:57:28.556094Z',
-        latestDate: '2018-06-29T17:57:28.556094Z',
-        requestedDate: '2018-06-15T17:57:28.556094Z'
-      },
-      paymentConditions: {
-        paymentDueDate: '2018-06-15T17:57:28.556094Z'
-      }
-    }
-  ];
+  transactions: TransactionDetails[];
 
   constructor() {
   }
 
   ngOnInit() {
+    this.transactions = this.getTransactions();
   }
 
   toggleSearch(): void {
     this.showSearch = !this.showSearch;
   }
 
+  getTransactions(): TransactionDetails[] {
+    return TRANSACTIONS;
+  }
   clickedItem(item: TransactionDetails): void {
     console.log('Transaction Component. Clicked on ' + JSON.stringify(item));
 
