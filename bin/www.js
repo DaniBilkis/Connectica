d1813ac831966def0352d9ea5ea3e27e63389b45
Dani Bilkis <dani.bilkis@gmail.com>
2018-06-21 15:57:43 -0500

    Merge branch 'April_24_2018' of https://github.com/DaniBilkis/Connectica into April_24_2018
    # Conflicts:
    #	src/app/app-routing/routes.ts

--- bin/www.js
+++ bin/www.js
@@ -25,7 +25,10 @@ const server = http.createServer(app);
  * Listen on provided port, on all network interfaces.
  */
 
-server.listen(port);
+server.listen( port, function () {
+  const expPort = server.address().port;
+  console.log("Express is working on port " + expPort);
+} );
 server.on('error', onError);
 server.on('listening', onListening);
 
